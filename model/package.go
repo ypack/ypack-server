b7af7622996fbea25251fa8a2475cd70dec21484
Javi <nhitbh@gmail.com>
2018-07-08 22:18:44 +0200

    Added missing json to models

--- model/package.go
+++ model/package.go
@@ -2,33 +2,35 @@ package model
 
 // Package to be installed into a system
 type Package struct {
-	ID          uint
-	Name        string    `json:"name"`
-	Description string    `json:"description"`
-	Website     string    `json:"website"`
-	Alias       []Alias   `json:"alias"`
-	Authors     []Author  `json:"authors"`
-	Versions    []Version `json:"versions"`
+	ID          uint    `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Website     string  `json:"website"`
+	Alias       []Alias `json:"alias"`
+	//Authors     []Author  `json:"authors"`
+	Versions []Version `json:"versions"`
 }
 
 // Author of a Package
 type Author struct {
-	ID      uint
+	ID      uint   `json:"id"`
 	Name    string `json:"name"`
 	Contact string `json:"contact"`
 }
 
 // Version of a Package
 type Version struct {
-	ID       uint
-	Name     string `json:"name"`
-	Url      string `json:"url"`
-	Checksum string `json:"checksum"`
-	OS       string `json:"os"`
-	Arch     string `json:"arch"`
+	ID        uint   `json:"id"`
+	PackageID uint   `json:"package_id"`
+	Name      string `json:"name"`
+	Url       string `json:"url"`
+	Checksum  string `json:"checksum"`
+	OS        string `json:"os"`
+	Arch      string `json:"arch"`
 }
 
 type Alias struct {
-	ID   uint
-	Name string `json:"name"`
+	ID        uint   `json:"id"`
+	Name      string `json:"name"`
+	PackageID uint   `json:"package_id"`
 }
