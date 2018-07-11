a6c7639951140c0ada0bb4256ddb024169f48764
Javi <nhitbh@gmail.com>
2018-07-11 23:45:41 +0200

    Check if the OS is empty when validate it

--- validate/validate.go
+++ validate/validate.go
@@ -13,6 +13,9 @@ var validOperatingSystems = []string{
 // IsValidOperatingSystem validate if the given OS is know or not. If the
 // OS is not valid this method returns an error, otherwise returns nil
 func IsValidOperatingSystem(os string) error {
+	if os == "" {
+		return nil
+	}
 	for i := 0; i < len(validOperatingSystems); i++ {
 		if validOperatingSystems[i] == os {
 			return nil
--- validate/validate_test.go
+++ validate/validate_test.go
@@ -11,4 +11,8 @@ func TestValidateOperatingSystem(t *testing.T) {
 	if err2 != nil {
 		t.Error("Expected nil error but got", err2)
 	}
+	err3 := IsValidOperatingSystem("")
+	if err3 != nil {
+		t.Error("Expected empty but got", err3)
+	}
 }
